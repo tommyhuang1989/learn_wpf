bb7b190e1ce5facb91cbe97b2c4b119563dfa189
tommy.huang <tommy.huang@easeware.net>
2020-03-05 14:41:11 +0800

    add painting and animation

---
+++ painting_and_animation/rectangle_window.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace painting_and_animation {
+    /// <summary>
+    /// Interaction logic for rectangle_window.xaml
+    /// </summary>
+    public partial class rectangle_window : Window {
+        public rectangle_window() {
+            InitializeComponent();
+        }
+    }
+}
